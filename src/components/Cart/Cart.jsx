367adf42088882059a700cf318af46171a9a8b09
Kevin Sauvage <kevinsauvage@outlook.com>
2023-04-08 19:28:58 +0200

    update page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,31 @@
 import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
 
+import apiHelper from '../../helpers/apiHelper';
 import useTotalPrice from '../../hooks/useTotalPrice';
+import getStripe from '../../utils/get-stripe';
 import Button from '../Button/Button';
 
 import styles from './Cart.module.scss';
 
 const Cart = () => {
-  const { items } = useSelector((state) => state.cart);
+  const { items, user } = useSelector((state) => state.cart);
   const total = useTotalPrice(items);
 
+  const { pathname, push } = useRouter();
+
+  const redirectToCheckout = async () => {
+    if (!user.user) return push('/login');
+
+    const response = await apiHelper('/api/checkout_sessions', {
+      items,
+      user: user.user,
+    });
+
+    if (!response || !response.id) return;
+    const stripe = await getStripe();
+    return stripe.redirectToCheckout({ sessionId: response.id });
+  };
   return (
     <div className={styles.Cart}>
       <div className={styles.header}>
@@ -24,7 +41,11 @@ const Cart = () => {
         ))}
       </div>
 
-      <Button href="/order" text="ORDER" />
+      {pathname?.startsWith('/cart') && items?.length > 0 ? (
+        <Button text="CONFIRM ORDER" onClick={redirectToCheckout} />
+      ) : (
+        <Button href="/cart" text="ORDER" />
+      )}
     </div>
   );
 };
