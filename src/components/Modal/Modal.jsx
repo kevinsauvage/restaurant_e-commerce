42fab13bd054859042a3afd27530ab7bbf06b07a
Kevin Sauvage <kevinsauvage@outlook.com>
2023-04-08 19:28:01 +0200

    improve modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,16 +1,14 @@
-import { useEffect, useState } from 'react';
 import { MdOutlineClose } from 'react-icons/md';
+
+import useHideScrollbar from '../../hooks/useHideScrollbar';
 
 import styles from './Modal.module.scss';
 
 const Modal = ({ children, handleClose }) => {
-  const [yPos, setYPos] = useState(0);
-
-  useEffect(() => setYPos(window.pageYOffset), []);
-
+  useHideScrollbar();
   return (
     <div className={styles.modal} onClick={handleClose} role="presentation">
-      <div className={styles.inner} style={{ top: yPos + 60 }}>
+      <div className={styles.inner}>
         <button onClick={handleClose} className={styles.btnClose} type="button">
           <MdOutlineClose />
         </button>
